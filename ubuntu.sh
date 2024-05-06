35c7a9afb7bac7e75b74fdef51a64f49b5ca3b88
COMEXR B.V <64539804+comexr@users.noreply.github.com>
2024-05-06 11:08:39 +0200

    Update dependencies

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt install cargo dbus rustc systemd make -y
+sudo apt install cargo dbus rustc systemd make pkg-config libssl-dev librust-lzma-sys-dev libdbus-1-dev -y
 make
 sudo make install
