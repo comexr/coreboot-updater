616e676c67065073d7b0d1eae49c4423316ea583
COMEXR B.V <64539804+comexr@users.noreply.github.com>
2024-05-07 15:59:40 +0200

    Update

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+#Install dependencies
 sudo apt install cargo dbus rustc systemd make pkg-config libssl-dev librust-lzma-sys-dev libdbus-1-dev -y
 make
 sudo make install
+
+#Prepare flash
+sudo system76-firmware-cli schedule
+
+#Copy firmware files
+sudo cp rom/ec.rom /boot/efi/system76-firmware-update/firmware
+sudo cp rom/firmware.rom /boot/efi/system76-firmware-update/firmware
+sudo rm -rf /boot/efi/system76-firmware-update/firmware/res
+
+#Remove dependencies
+sudo make uninstall
+
+#Start flash
+sudo reboot
